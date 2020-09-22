5b1e558b95b01e83dda6885e5ac294b3e20ac74e
theodorous beatayuvens <theochandra27@gmail.com>
2020-09-22 21:04:08 +0700

    add unit test for domain module

---
+++ domain/src/test/java/com/android/domain/usecase/GetSearchedUsersUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.android.domain.usecase
+
+import com.android.domain.model.User
+import com.android.domain.repository.UserRepository
+import com.android.domain.rx.RxJavaTestHooksResetRule
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+import com.android.domain.usecase.GetSearchedUsersUseCase.Result.Loading
+import com.android.domain.usecase.GetSearchedUsersUseCase.Result.Success
+import com.android.domain.usecase.GetSearchedUsersUseCase.Result.Failure
+
+@RunWith(MockitoJUnitRunner::class)
+class GetSearchedUsersUseCaseTest {
+
+    @get:Rule
+    var rxJavaTestHooksResetRule = RxJavaTestHooksResetRule()
+
+    @Mock lateinit var userRepository: UserRepository
+
+    private lateinit var sut: GetSearchedUsersUseCase
+
+    @Before
+    fun setup() {
+        sut = GetSearchedUsersUseCase(userRepository)
+    }
+
+    @Test
+    fun `get searched user`() {
+        given(userRepository.getSearchedUsers(any(), any(), any()))
+            .willReturn(Single.error(Throwable()))
+
+        sut.execute("theo", 1, 10).test()
+
+        verify(userRepository).getSearchedUsers(any(), any(), any())
+    }
+
+    @Test
+    fun `shows loading when start`() {
+        given(userRepository.getSearchedUsers(any(), any(), any()))
+            .willReturn(Single.just(mock()))
+
+        sut.execute("theo", 1, 10).test()
+            .assertValueAt(0) { it == Loading }
+    }
+
+    @Test
+    fun `returns the success result when success get data`() {
+        val searchedUsers = arrayListOf<User>()
+
+        given(userRepository.getSearchedUsers(any(), any(), any()))
+            .willReturn(Single.just(searchedUsers))
+
+        sut.execute("theo", 1, 10).test()
+            .assertValueAt(1) { (it as Success).users == searchedUsers }
+    }
+
+    @Test
+    fun `returns the failure result when error get data`() {
+        val throwable = Throwable()
+
+        given(userRepository.getSearchedUsers(any(), any(), any()))
+            .willReturn(Single.error(throwable))
+
+        sut.execute("theo", 1, 10).test()
+            .assertValueAt(1) { (it as Failure).throwable == throwable }
+    }
+
+}
